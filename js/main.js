39cc50b57e2d249b5cfc50e447f56d51be8e24a2
denzildoyle <yell@denzildoyle.me>
2016-03-04 12:48:23 -0400

    Add css animation

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+    $('#hero-title').addClass('animated fadeIn');
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#hero-title").addClass("animated fadeIn")});
